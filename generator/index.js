af6f078528ba57995582dda40d1a81fc4baaada4
NataliaTepluhina <tarya.se@gmail.com>
2018-11-17 21:18:53 +0200

    * added greet npm script

--- generator/index.js
+++ generator/index.js
@@ -5,27 +5,30 @@ module.exports = api => {
     },
     devDependencies: {
       'vue-auto-routing': '^0.3.0'
+    },
+    scripts: {
+      greet: 'vue-cli-service greet'
     }
-  })
+  });
 
-  api.render('./template')
+  api.render('./template');
 
   if (api.invoking) {
     api.postProcessFiles(files => {
       Object.keys(files).forEach(name => {
         if (/^src\/views[/$]/.test(name)) {
-          delete files[name]
+          delete files[name];
         }
-      })
-    })
+      });
+    });
 
     if (api.hasPlugin('typescript')) {
       api.postProcessFiles(files => {
-        delete files['src/router.ts']
-      })
+        delete files['src/router.ts'];
+      });
 
-      const convertFiles = require('@vue/cli-plugin-typescript/generator/convert')
-      convertFiles(api)
+      const convertFiles = require('@vue/cli-plugin-typescript/generator/convert');
+      convertFiles(api);
     }
   }
-}
+};
