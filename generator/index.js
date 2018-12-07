feea0c670af2863c960bd1e070106f3b0f85b868
Natalia Tepluhina <tarya.se@gmail.com>
2018-12-07 21:29:31 +0200

    *added import router to the main.js

--- generator/index.js
+++ generator/index.js
@@ -11,6 +11,27 @@ module.exports = (api, options) => {
     }
   });
 
+  const routerLine = `\nimport router from './router';`;
+
+  api.onCreateComplete(() => {
+    // inject to main.js
+    const fs = require('fs');
+    const ext = api.hasPlugin('typescript') ? 'ts' : 'js';
+    const mainPath = api.resolve(`./src/main.${ext}`);
+
+    // get content
+    let contentMain = fs.readFileSync(mainPath, { encoding: 'utf-8' });
+    const lines = contentMain.split(/\r?\n/g).reverse();
+
+    // inject import
+    const lastImportIndex = lines.findIndex(line => line.match(/^import/));
+    lines[lastImportIndex] += routerLine;
+
+    // modify app
+    contentMain = lines.reverse().join('\n');
+    fs.writeFileSync(mainPath, contentMain, { encoding: 'utf-8' });
+  });
+
   if (options.addExampleRoutes) {
     api.render('./template', {
       ...options
